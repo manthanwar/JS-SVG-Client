1ec7a777d1c89dbc549a8cff26f2ab7931736ebe
Amit M. Manthanwar <manthanwar@hotmail.com>
2024-11-25 01:36:09 +0100

    updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
  },
  optimization: {
   minimize: true,
-  minimizer: [new TerserPlugin({ parallel: true })]
+  minimizer: [new TerserPlugin()]
  },
  mode: 'development',
  mode: 'production'
