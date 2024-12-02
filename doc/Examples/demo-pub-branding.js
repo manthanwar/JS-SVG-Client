fac0cbccd1db6a108c965cfc7f6c70472a381a3d
Amit M. Manthanwar <manthanwar@hotmail.com>
2024-12-02 01:04:12 +0100

    aded doc/Examples/amm-pst-business-card.tex

--- doc/Examples/demo-pub-branding.js
+++ doc/Examples/demo-pub-branding.js
@@ -21,33 +21,34 @@ window.onload = (event) => {
  // main.style.border = '2px solid red;';
 
  article.innerHTML = `
-<h3>Brand Identity</h3>
+<h3>Brand Identity and House Style</h3>
 <ul>
 <li><a href="demo-art-logo.pdf" target="_blank">Logo Art Design</a></li>
-<li><a href="#" target="_blank">Business Card</a> <i>coming soon...</i></li>
+<li><a href="amm-pst-letter.pdf" target="_blank">Formal Letter</a></li>
+<li><a href="amm-pst-talk.pdf" target="_blank">Presentation</a></li>
+<li><a href="amm-pst-business-card.pdf" target="_blank">Business Card</a></li>
 </ul>
 
-<h3>E-Commerce</h3>
+<h3>E-Commerce Applications</h3>
 <ul>
 <li><a href="amm-pst-quotation.pdf" target="_blank">Quotation</a></li>
 <li><a href="amm-pst-invoice.pdf" target="_blank">Invoice</a></li>
 <li><a href="amm-pst-receipt.pdf" target="_blank">Receipt</a></li>
 </ul>
 
-<h3>House Style</h3>
+<h3>Reports and Other Publications <i style="font-size:16px">coming soon...</i></h3>
 <ul>
-<li><a href="amm-pst-letter.pdf" target="_blank">Formal Letter</a></li>
-<li><a href="#" target="_blank">Presentation</a> <i>coming soon...</i></li>
-<li><a href="#" target="_blank">Reports</a> <i>coming soon...</i></li>
-<li><a href="#" target="_blank">Newsletters</a> <i>coming soon...</i></li>
-<li><a href="#" target="_blank">infographic</a> <i>coming soon...</i></li>
+<li><a href="#" target="_blank">Reports</a></li>
+<li><a href="#" target="_blank">Newsletters</a></li>
+<li><a href="#" target="_blank">infographic</a> </li>
 </ul>
 
-<h3>Awards and Recognitions</h3>
+
+<h3>Awards and Recognitions <i style="font-size:16px">coming soon...</i></h3>
 <ul>
-<li><a href="#" target="_blank">Employee of the Month</a> <i>coming soon...</i></li>
-<li><a href="#" target="_blank">Certificate of Award</a> <i>coming soon...</i></li>
-<li><a href="#" target="_blank">Certificate of Training</a> <i>coming soon...</i></li>
+<li><a href="#" target="_blank">Employee of the Month</a></li>
+<li><a href="#" target="_blank">Certificate of Award</a></li>
+<li><a href="#" target="_blank">Certificate of Training</a></li>
 </ul>
 `;
 };
