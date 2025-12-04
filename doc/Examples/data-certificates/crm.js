d5ae63643b5cefa1ff5e93f6eff7fe0cb2111003
Amit Manohar Manthanwar <manthanwar@hotmail.com>
2025-12-04 15:19:47 +0100

    added doc/Examples/data-certificates/crm.js

---
+++ doc/Examples/data-certificates/crm.js
@@ -0,0 +1,160 @@
+const crm = {
+  product: [
+    {
+      id: "product 1",
+      name: "Dolphin DPR",
+      url: "link-to-website",
+      flyer: "link-to-pdf",
+      brochure: "link-to-pdf",
+      proposal: "link-to-pdf",
+      pricing: "link-to-pdf",
+    },
+    {
+      id: "product 2",
+      name: "Dolphin Certificate",
+      url: "link-to-website",
+      flyer: "link-to-pdf",
+      brochure: "link-to-pdf",
+      proposal: "link-to-pdf",
+      pricing: "link-to-pdf",
+    },
+    {
+      id: "product 3",
+      name: "Dolphin EdTech",
+      url: "link-to-website",
+      flyer: "link-to-pdf",
+      brochure: "link-to-pdf",
+      proposal: "link-to-pdf",
+      pricing: "link-to-pdf",
+    },
+    {
+      id: "product 4",
+      name: "Dolphin FinTech",
+      url: "link-to-website",
+      flyer: "link-to-pdf",
+      brochure: "link-to-pdf",
+      proposal: "link-to-pdf",
+      pricing: "link-to-pdf",
+    },
+  ],
+  seller: [
+    { name: "Chandra Mohan", email: "", phone: "", id: "002" },
+    { name: "", email: "", phone: "", id: "" },
+    { name: "", email: "", phone: "", id: "" },
+    { name: "James Bond", email: "", phone: "", id: "007" },
+  ],
+  buyer: [
+    {
+      id: "buyer - school - 001",
+      organisation: {
+        name: "",
+        address: {
+          street: "",
+          city: "",
+          state: "",
+          postcode: "",
+          country: "",
+        },
+      },
+      meeting: [
+        {
+          date: "yyyy-mm-dd hh:ss",
+          present: [
+            { name: "", email: "" },
+            { name: "", email: "" },
+          ],
+          scriber: [{ name: "", email: "" }],
+          agenda: ["item 1", "item 2", "item 3"],
+          outcome: ["outcome 1", "outcome 2"],
+          action: ["action 1", "action 2"],
+          question: [
+            { question: "", answer: "" },
+            { question: "", answer: "" },
+          ],
+        },
+        {
+          date: "yyyy-mm-dd hh:ss",
+          agenda: "",
+          outcome: "",
+          actions: [],
+        },
+      ],
+      contact: [
+        {
+          name: "",
+          email: "",
+          phone: "",
+          interest: "product they want",
+          isDecisionMaker: "no",
+        },
+        {
+          name: "",
+          email: "",
+          phone: "",
+          organisation: "",
+          interest: "product they want",
+          isDecisionMaker: "yes",
+        },
+      ],
+      stage:
+        "1. Awareness, 2. Discovery, 3. Evaluation, 4. Intent, 5. Purchase, 6. Loyalty",
+      account: ["seller id - manager 1", "seller id - manager 2"],
+    },
+    {
+      id: "buyer - bank - 001",
+      organisation: {
+        name: "",
+        address: {
+          street: "",
+          city: "",
+          state: "",
+          postcode: "",
+          country: "",
+        },
+      },
+      meeting: [
+        {
+          date: "yyyy-mm-dd hh:ss",
+          present: [
+            { name: "", email: "" },
+            { name: "", email: "" },
+          ],
+          scriber: [{ name: "", email: "" }],
+          agenda: ["item 1", "item 2", "item 3"],
+          outcome: ["outcome 1", "outcome 2"],
+          action: ["action 1", "action 2"],
+          question: [
+            { question: "", answer: "" },
+            { question: "", answer: "" },
+          ],
+        },
+        {
+          date: "yyyy-mm-dd hh:ss",
+          agenda: "",
+          outcome: "",
+          actions: [],
+        },
+      ],
+      contact: [
+        {
+          name: "",
+          email: "",
+          phone: "",
+          interest: "product they want",
+          isDecisionMaker: "no",
+        },
+        {
+          name: "",
+          email: "",
+          phone: "",
+          organisation: "",
+          interest: "product they want",
+          isDecisionMaker: "yes",
+        },
+      ],
+      stage:
+        "1. Awareness, 2. Discovery, 3. Evaluation, 4. Intent, 5. Purchase, 6. Loyalty",
+      account: ["seller id - manager 1", "seller id - manager 2"],
+    },
+  ],
+};
