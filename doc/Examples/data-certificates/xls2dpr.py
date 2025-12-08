c6da7c29e6a26904f7ffc40041a0407ee6cf2372
Amit Manohar Manthanwar <manthanwar@hotmail.com>
2025-12-08 18:28:12 +0100

    changed python shebang to linux

--- doc/Examples/data-certificates/xls2dpr.py
+++ doc/Examples/data-certificates/xls2dpr.py
@@ -1,4 +1,5 @@
-#! /usr/bin/env python3
+#! /usr/bin/python3
+#!/usr/bin/env python3
 #! /c/Users/amit/AppData/Local/Programs/Python/Python312/python
 # ==============================================================================
 # File Name     : <xls2dpr.py>
--- doc/Examples/data-certificates/xls2tex.py
+++ doc/Examples/data-certificates/xls2tex.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python3
 #! /usr/bin/env python3
 #! /c/Users/amit/AppData/Local/Programs/Python/Python312/python
 # ==============================================================================
