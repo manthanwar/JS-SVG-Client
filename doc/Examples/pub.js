309dd32a0595972c2049d97efac3b98c789df817
Amit Manohar Manthanwar <manthanwar@hotmail.com>
2026-05-30 12:46:01 +0200

    updated doc/Examples/pub.js

--- doc/Examples/pub.js
+++ doc/Examples/pub.js
@@ -2,7 +2,7 @@ import * as mySvg from '../svg.min.js';
 import { Utils } from '../Utils.js';
 import dataTemplate from '../demo-data-template.js';
 window.onload = (event) => {
- document.title = 'Products';
+ document.title = 'Publications';
  dataTemplate.renderBody('pub.html');
  document.getElementById('page').innerHTML = 'Publications';
 
@@ -13,43 +13,40 @@ window.onload = (event) => {
  article.innerHTML = `
 
 <h3>Articles</h3>
+
 <ul>
-${Utils.createListItem('', 'Fuel Cell Review - <i>Under Preparation</i>')}
+<li>Fuel Cell Fundamentals</li>
+<ul>
+${Utils.createListItem('', 'Literature Review - <i>Under Preparation</i>')}
+${Utils.createListItem('PEMFC_Functional_Map_Part_1_Technique_v1.10.pdf', 'Experimental Map: 1. Temperature Profiles')}
+${Utils.createListItem('PEMFC_Map_P02_v3.pdf', 'Experimental Map: 2. Temperature Profiles with Uncertainty')}
+${Utils.createListItem('Spatially_resolved_oxygen_reaction_water.pdf', 'Experimental Map: 3. Reactants, Water, and Temperature Profiles')}
+</ul>
 
-${Utils.createListItem('PEMFC_Functional_Map_Part_1_Technique_v1.10.pdf', 'Fuel Cell Experimental Map: 1. Temperature Profiles')}
+<li>Fuel Cell Strategies</li>
+<ul>
+${Utils.createListItem('PEM1kW_Model_Heat_Management_v2.pdf', 'Operational Strategy: 1. Dynamic Model of Fuel Cell Energy System')}
+${Utils.createListItem('Smart_H2Energy_v02_review_submitted.pdf', 'Operational Strategy: 2. Smart Operational Strategies for Fuel Cell Energy System')}
+${Utils.createListItem('ESCAPE25_PSE2015_Imperial_MKP_Paper_v2.pdf', 'Operational Strategy: 3. Explicit Control of Mini Fuel Cell Vehicle')}
+</ul>
 
-${Utils.createListItem('PEMFC_Map_P02_v3.pdf', 'Fuel Cell Experimental Map: 2. Temperature Profiles with Uncertainty')}
+<li>Control Theory</li>
+<ul>
+${Utils.createListItem('Robust-Control-Polytopic.pdf', 'Robust Control: 1. Polytopically Systems')}
+${Utils.createListItem('Robust-Control-Hybrid.pdf', 'Robust Control: 2. Hybrid  Systems')}
+${Utils.createListItem('Robust-Control-Hybrid-ESCAPE.pdf', 'Robust Control: 3. Hybrid Uncertain Systems')}
+</ul>
+</ul>
 
-${Utils.createListItem('Spatially_resolved_oxygen_reaction_water.pdf', 'Fuel Cell Experimental Map: 3. Reactants, Water, and Temperature Profiles')}
-
-${Utils.createListItem('Smart_H2Energy_v02_review_submitted.pdf', 'Smart Operational Strategies for Fuel Cell Energy System')}
-
-${Utils.createListItem('PEM1kW_Model_Heat_Management_v2.pdf', 'Dynamic Model of Fuel Cell Energy System')}
-
-
-${Utils.createListItem('ESCAPE25_PSE2015_Imperial_MKP_Paper_v2.pdf', 'Explicit Control of Mini Fuel Cell Vehicle')}
-
-
-
-${Utils.createListItem('Robust-Control-Polytopic.pdf', 'Robust Control of Polytopically Uncertain Systems')}
-
-${Utils.createListItem('Robust-Control-Hybrid.pdf', 'Robust Control: 1. Hybrid Uncertain Systems')}
-
-${Utils.createListItem('Robust-Control-Hybrid-ESCAPE.pdf', 'Robust Control: 1. Hybrid Uncertain Systems 2')}
-
+<h3>Technical Reports</h3>
+<ul>
+${Utils.createListItem('PEMFC-Lab-Manual.pdf', 'Fuel Cell Lab Manual')}
 </ul>
 
 <h3>Theses</h3>
 <ul>
 ${Utils.createListItem('PhD_Thesis_Amit_M_Manthanwar_e2_submitted.pdf', 'Thesis Version 1')}
 ${Utils.createListItem('', 'Thesis Version 2 - <i>Under Preparation</i>')}
-
-</ul>
-
-
-<h3>Technical Reports</h3>
-<ul>
-${Utils.createListItem('PEMFC-Lab-Manual.pdf', 'Fuel Cell Lab Manual')}
 </ul>
 
  `;
