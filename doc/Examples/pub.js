dcb2dd9a660d74ae4beea284489be4663b80de75
Amit Manohar Manthanwar <manthanwar@hotmail.com>
2026-06-22 09:14:57 +0200

    updated pub

--- doc/Examples/pub.js
+++ doc/Examples/pub.js
@@ -57,14 +57,15 @@ ${Utils.createListItem("fig-fc-multiscale.pdf", "Fuel Cell Multi-Scale")}
 ${Utils.createListItem("fig-fc-h2-network.pdf", "Hydrogen Supply Chain")}
 
 ${Utils.createListItem("fig-fc-map-temp-array-50cm2.pdf", "Fuel Cell Map - Temperature Array")}
-
 ${Utils.createListItem("fig-fc-system-pfd.pdf", "Fuel Cell System PFD")}
 
+${Utils.createListItem("fig-smart-automation.pdf", "Smart Automation")}
 ${Utils.createListItem("fig-fc-mpc-deployment-flowchart.pdf", "Fuel Cell Control Deployment")}
 
 ${Utils.createListItem("fig-fc-car-architecture.pdf", "Fuel Cell Car Architecture")}
 ${Utils.createListItem("fig-fc-car-kinematics.pdf", "Car Kinematics")}
 ${Utils.createListItem("fig-fc-car-body.pdf", "Car Body")}
+${Utils.createListItem("fig-motor-dc.pdf", "DC Motor")}
 
 </ul>
 
